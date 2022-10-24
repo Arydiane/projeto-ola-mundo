8c678c7064241bc38d3fb1369289bb0109c202bd
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-10-24 13:53:08 -0300

    atualiza componente post
    Passa a exibir posts recomendados no fim da página

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Post.css'
+import styles from './Post.module.css'
 import { useParams } from 'react-router-dom'
 import posts from 'json/posts.json'
 import PostModelo from 'componentes/PostModelo';
 import ReactMarkdown from 'react-markdown';
 import PaginaPadrao from 'componentes/PaginaPadrao';
 import NaoEncontrada from 'paginas/NaoEncontrada';
+import PostCard from 'componentes/PostCard'
+
 
 export default function Post() {
 
@@ -21,6 +24,11 @@ export default function Post() {
     )
   }
 
+  const postsRecomendados = posts
+  .filter((post)=> post.id !== Number(parametros.id))
+  .sort((a,b) => b.id - a.id)
+  .slice(0, 4);
+
   return (
     <PaginaPadrao>
       <PostModelo
@@ -32,7 +40,18 @@ export default function Post() {
             {post.texto}
           </ReactMarkdown>
         </div>
+        <h2 className={styles.tituloOutrosPosts}>
+          Outros posts que você pode gostar:
+        </h2>
+        <ul className={styles.postsRecomendados}>
+          {postsRecomendados.map((post) => (
+            <li key={post.id}>
+              <PostCard  post={post}/>
+            </li>
+          ))}
+        </ul>
       </PostModelo>
+
     </PaginaPadrao>
 
   )
